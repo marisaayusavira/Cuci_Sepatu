a401dfe92b97226f0b3794b7a55d1cbbf811dce6
Gema <gemafajar09@gmail.com>
2019-12-21 17:09:40 +0700

    Cucisepatu

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include"koneksi.php" ?>
+<?php include"template/header.php" ?>
+<?php include"template/menu.php" ?>
+
+
+<?php include"template/footer.php" ?>
---
+++ template/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?= $base_url ?>asset/plugins/summernote/summernote-bs4.css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+      &nbsp;
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <img src="<?= $base_url ?>/asset/dist/img/user2-160x160.jpg" width="30px" class="img-circle elevation-" alt="User Image">
+        </a>
+         <div style="position:center;" class="dropdown-menu dropdown-menu-xs dropdown-menu-right">
+            <!-- <div class="dropdown-divider"></div> -->
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-envelope">&nbsp;&nbsp;&nbsp;Profil</i>
+            </a>
+            <!-- <div class="dropdown-divider"></div> -->
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-sign-out-alt">&nbsp;&nbsp;&nbsp;Log out</i>
+            </a>
+         </div>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
